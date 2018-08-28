9a3690099ec4cfbb32574b2462ba971bbf46c77b
dilip29 <dsp29.nitm@gmail.com>
2018-08-29 01:27:41 +0530

    circle file updated

--- CS 362 Computer Graphics/circle.c
+++ CS 362 Computer Graphics/circle.c
@@ -6,7 +6,7 @@
 
 
 
-void half_circle(int xc,int yc,int r)
+void half_upper_circle(int xc,int yc,int r)
 {
 int p,x=0,y=r;
 p=1-r;
@@ -30,6 +30,49 @@ while(x<y)
 
     putpixel(xc-x,yc-y,5);
     putpixel(xc-y,yc-x,5);
+
+    delay(30);
+
+}
+
+
+
+delay(50);
+}
+
+
+
+void half_lower_circle(int xc,int yc,int r)
+{
+int p,x=0,y=r;
+p=1-r;
+
+while(x<y)
+{
+	x++;
+    if (p<0)
+	{
+	 p=p+2*x+1;
+	}
+    else
+	{
+	y--;
+	p=p+2*x+1-2*y;
+
+	}
+
+
+
+
+
+
+    	putpixel(xc-x,yc+y,5);
+	putpixel(xc-y,yc+x,5);
+
+	putpixel(xc+x,yc+y,5);
+	putpixel(xc+y,yc+x,5);
+
+
     delay(30);
 
 }
@@ -92,8 +135,8 @@ void main( )
     scanf("%d",&r);
 
     full_circle(xc,yc,r);
-    half_circle(xc-2*r,yc,r);
-    half_circle(xc+2*r,yc,r);
+    half_upper_circle(xc-2*r,yc,r);
+    half_lower_circle(xc+2*r,yc,r);
     closegraph();
 
 }
