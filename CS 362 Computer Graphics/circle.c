edf761e1f530a486c4c6688c71c2606668aadc1f
dilip29 <dsp29.nitm@gmail.com>
2018-08-29 20:45:14 +0530

    circle created using gimp image file

--- CS 362 Computer Graphics/circle.c
+++ CS 362 Computer Graphics/circle.c
@@ -1,9 +1,8 @@
-
-#include <graphics.h>
 #include <stdio.h>
 #include <math.h>
-#include <dos.h>
 
+int l=500,b=500;
+int mat[500][500]={0};
 
 
 void half_upper_circle(int xc,int yc,int r)
@@ -25,19 +24,22 @@ while(x<y)
 
 	}
 
-    putpixel(xc+x,yc-y,5);
-    putpixel(xc+y,yc-x,5);
 
-    putpixel(xc-x,yc-y,5);
-    putpixel(xc-y,yc-x,5);
 
-    delay(30);
+
+    mat[yc-y][xc+x]=255;
+    mat[yc-x][xc+y]=255;
+
+    mat[yc-y][xc-x]=255;
+    mat[yc-x][xc-y]=255;
+
+    //delay(30);
 
 }
 
 
 
-delay(50);
+//delay(50);
 }
 
 
@@ -62,27 +64,21 @@ while(x<y)
 	}
 
 
+    mat[yc+y][xc-x]=255;
+	mat[yc+x][xc-y]=255;
+
+	mat[yc+y][xc+x]=255;
+	mat[yc+x][xc+y]=255;
 
 
-
-
-    	putpixel(xc-x,yc+y,5);
-	putpixel(xc-y,yc+x,5);
-
-	putpixel(xc+x,yc+y,5);
-	putpixel(xc+y,yc+x,5);
-
-
-    delay(30);
+    //delay(30);
 
 }
 
 
 
-delay(50);
+//delay(50);
 }
-
-
 
 
 void full_circle(int xc,int yc,int r)
@@ -90,7 +86,7 @@ void full_circle(int xc,int yc,int r)
 int p,x=0,y=r;
 p=1-r;
 
-while(x<y)
+while(x<=y)
 {
 	x++;
     if (p<0)
@@ -103,31 +99,30 @@ while(x<y)
 	p=p+2*x+1-2*y;
 
 	}
-    putpixel(xc+x,yc+y,5);
-    putpixel(xc+x,yc-y,5);
-    putpixel(xc-x,yc+y,5);
-    putpixel(xc-x,yc-y,5);
 
-    putpixel(xc+y,yc+x,5);
-    putpixel(xc+y,yc-x,5);
-    putpixel(xc-y,yc+x,5);
-    putpixel(xc-y,yc-x,5);
-    delay(30);
+    mat[xc+x][yc+y]=255;
+    mat[xc+x][yc-y]=255;
+    mat[xc-x][yc+y]=255;
+    mat[xc-x][yc-y]=255;
+
+    mat[xc+y][yc+x]=255;
+    mat[xc+y][yc-x]=255;
+    mat[xc-y][yc+x]=255;
+    mat[xc-y][yc-x]=255;
+
 
 }
 
 
 
-delay(50);
+//delay(50);
 }
 
 
 void main( )
 {
     int xc,yc,r;
-    int i,gd=DETECT,gm;
 
-    initgraph(&gd,&gm,"C:\\TURBOC3\\BGI");
 
     printf("Enter the center xc and yc ");
     scanf("%d%d",&xc,&yc);
@@ -137,7 +132,37 @@ void main( )
     full_circle(xc,yc,r);
     half_upper_circle(xc-2*r,yc,r);
     half_lower_circle(xc+2*r,yc,r);
-    closegraph();
+
+    int x1=xc-3*r,x2=xc+3*r;
+    while(x1<=x2)
+    {
+        mat[yc][x1]=255;
+        x1++;
+    }
+   //mat[xc][yc-2*r]=255;
+
+
+    int i,j,temp;
+    FILE* pgmimg;
+    pgmimg=fopen("pgmimg.pgm","wb");
+
+    fprintf(pgmimg,"P2\n");
+    fprintf(pgmimg,"%d %d\n",b,l);
+
+    fprintf(pgmimg,"255\n");
+
+
+
+    for (i = 0; i < l; i++) {
+        for (j = 0; j < b; j++) {
+            temp = mat[i][j];
+
+            // Writing the gray values in the 2D array to the file
+            fprintf(pgmimg, "%d ", temp);
+        }
+        fprintf(pgmimg, "\n");
+    }
+    fclose(pgmimg);
+
 
 }
-
